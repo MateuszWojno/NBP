34a960e72374f60cbc9f614cfff1ebeed4aabccc
MateuszWojno <mateusz.wojno9922@gmail.com>
2023-06-21 12:38:45 +0200

    Iplementation logic responsible  for curreny denomination

--- currencyDenomination.php
+++ currencyDenomination.php
@@ -2,25 +2,42 @@
 
 require_once 'src/autoload.php';
 
+use NBP\Application\CurrencyDenomination;
+use NBP\Http\Request\CurrencyDenominationRequest;
 use NBP\Persistence\ConnectionString;
 use NBP\Persistence\CredentialsFile;
+use NBP\Persistence\Database\CurrencyDenominationRepository;
 use NBP\Persistence\Database\CurrencyRateReadRepository;
-use NBP\View\View;
-use NBP\Http\Request\CurrencyDenominationRequest;
+use NBP\View\Validation;
+use NBP\View\Views\CurrencyDenominationView;
 
 $string = new ConnectionString(new CredentialsFile("connection.txt"));
 $pdo = $string->getPdo();
 
-function getView(CurrencyRateReadRepository $currencyRateReadRepository, CurrencyDenominationRequest $request): View
+function getView(CurrencyRateReadRepository $currencyRateReadRepository, CurrencyDenominationRequest $request, CurrencyDenomination $currencyDenomination, CurrencyDenominationRepository $denominationRepository)
 {
-    $currencyRates = $currencyRateReadRepository->fetchCurrencyRates();
-    return new View('src/NBP/View/pages/currencyDenomination.phtml', [
-        'currencyRates' => $currencyRates,
-    ]);
 
+    if (!$request->isConvert()) {
+        return new CurrencyDenominationView($currencyRateReadRepository->fetchCurrencyRates(), null, Validation::success());
+    }
+
+    $startingCurrency = $request->startingCurrency();
+    list($midStarting, $currencyStarting) = explode('|', $startingCurrency);
+    $targetCurrency = $request->targetCurrency();
+    list($midTarget, $currencyTarget) = explode('|', $targetCurrency);
+
+    if ($request->amount() === '') {
+        return new CurrencyDenominationView($currencyRateReadRepository->fetchCurrencyRates(), null, Validation::failure('amount', 'Empty field'));
+    }
+
+    if (!is_numeric($request->amount())) {
+        return new CurrencyDenominationView($currencyRateReadRepository->fetchCurrencyRates(), null, Validation::failure('amount', 'Provide amount'));
+    }
+    $denominationRepository->addCurrencyDenomination($currencyStarting, $currencyTarget, $request->amount(), date('y-m-d'));
+    return new CurrencyDenominationView($currencyRateReadRepository->fetchCurrencyRates(), $currencyDenomination->convert($midStarting, $midTarget, $request->amount()), Validation::success());
 }
 
-$getView = getView(new CurrencyRateReadRepository($pdo), new CurrencyDenominationRequest($_POST));
+$getView = getView(new CurrencyRateReadRepository($pdo), new CurrencyDenominationRequest($_POST), new CurrencyDenomination(), new CurrencyDenominationRepository($pdo));
 $getView->render();
 
 
